90ccf7b2d9c2620ea7115b10cbaf8c18c213be15
Jared Myers <jamy3873@colorado.edu>
2019-11-15 22:23:30 -0700

    improved ghost movement

--- reflection-prototype/Assets/__Scripts/GhostBehavior.cs
+++ reflection-prototype/Assets/__Scripts/GhostBehavior.cs
@@ -6,7 +6,6 @@ public class GhostBehavior : MonoBehaviour
 {
     private Ray groundRay;
     public GameObject targetObject;
-    private Vector3 targetPos;
     private Rigidbody rb;
     private float startTime;
     private float y0;
@@ -26,22 +25,58 @@ public class GhostBehavior : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+    }
+
+    private void FixedUpdate()
+    {
         Move();
     }
 
     void Move()
     {
-        if (targetObject)
-        {
-            targetPos = targetObject.transform.position;
-            transform.position = Vector3.MoveTowards(transform.position, targetPos, speed * 0.05f);
-        }
-
         Vector3 tempPos = transform.position;
         float age = Time.time - startTime;
         float theta = Mathf.PI * 2 * age / floatSpeed;
         float sin = Mathf.Sin(theta);
         tempPos.y = y0 + floatHeight * sin;
         transform.position = tempPos;
+
+        if (targetInSight(targetObject))
+        {
+            Vector3 targetPos = targetObject.transform.position;
+            Vector3 targetDir = targetPos - transform.position;
+            transform.position = Vector3.MoveTowards(transform.position, targetPos, speed * 0.05f);
+            Quaternion toRotation = Quaternion.LookRotation(targetDir, transform.up);
+            /*print("Target:" + targetDir.ToString());
+            print("Rotate:"+toRotation.ToString());*/
+            Quaternion rot = Quaternion.Lerp(transform.rotation, toRotation, 100f);
+            transform.rotation = rot;
+        }
+
+        
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawRay(new Ray(transform.position, transform.forward));
+    }
+
+    bool targetInSight(GameObject target)
+    {
+        Vector3 targetDir = (target.transform.position - transform.position).normalized;
+        if (Vector3.Angle(transform.forward, targetDir) < 60)
+        {
+            return true;
+        }
+
+        return false;
+        /*Ray sightline = new Ray(transform.position, targetDir);
+        RaycastHit hit;
+        if (Physics.Raycast(sightline, out hit, 20))
+        {
+            
+        }*/
     }
 }
